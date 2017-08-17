507bed1f79d416d478a9fce9958b00ba56353608
Jayan <smrjay001@myuct.ac.za>
2017-08-17 13:07:35 +0200

    Initial LibGdx Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# TetrisGame
-This is a take on Tetris written for my Game Development Course at UCT. It is written using the LibGDX framework.
---
+++ desktop/src/smrjay001/csc2003s/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package smrjay001.csc2003s.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import smrjay001.csc2003s.tetris.TetrisGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrisGame(), config);
+	}
+}
